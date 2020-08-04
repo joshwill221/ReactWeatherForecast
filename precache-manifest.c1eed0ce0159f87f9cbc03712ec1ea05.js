b07ad13e224be26aca03f74fbeb32a88c1ffd5c9
Josh Williams <joshwill221@gmail.com>
2020-08-04 14:06:18 +0800

    Updates

---
+++ precache-manifest.c1eed0ce0159f87f9cbc03712ec1ea05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942347f696ea1ce3b3ffc286a98967b7",
+    "url": "/ReactWeatherForecast/index.html"
+  },
+  {
+    "revision": "48cbd8db737a2e1eb47e",
+    "url": "/ReactWeatherForecast/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "41631b90d6e666fb0c23",
+    "url": "/ReactWeatherForecast/static/css/main.2b6e88f1.chunk.css"
+  },
+  {
+    "revision": "48cbd8db737a2e1eb47e",
+    "url": "/ReactWeatherForecast/static/js/2.4aa00608.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWeatherForecast/static/js/2.4aa00608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41631b90d6e666fb0c23",
+    "url": "/ReactWeatherForecast/static/js/main.f076bc1d.chunk.js"
+  },
+  {
+    "revision": "aeb34f22d01c36ae592f",
+    "url": "/ReactWeatherForecast/static/js/runtime-main.4d4be970.js"
+  }
+]);
